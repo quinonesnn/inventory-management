5faa231c0fe5829dd33de70259e7b0a32bdd87b7
quinonesnn <quinonesnn@gmail.com>
2024-08-26 13:20:26 -0400

    Connected backend to frontend to create Dashboard Metrics

---
+++ server/src/rotues/dashboard.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";;
+import { getDashboardMetrics } from "../controllers/dashboard.controller";
+
+
+const router = Router();
+
+router.get("/", getDashboardMetrics);
+
+export default router;
