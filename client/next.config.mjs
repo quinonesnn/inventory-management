7324de0a4021011e7b6b4ea4b0298955d23dadf7
quinonesnn <quinonesnn@gmail.com>
2024-08-30 14:05:16 -0400

    config image  modification

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "https://s3--inventorymanagement.s3.amazonaws.com",
-                port: "",
-                pathname: "/**",
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3--inventorymanagement.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
